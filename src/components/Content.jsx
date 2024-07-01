2c5236ccf78a5c2d78749fbcdac18896c359d498
Muhammad Jahanzeb <i.am.jahanzeb6@gmail.com>
2024-07-01 20:46:54 +0500

    create list item

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,19 +1,83 @@
+import { useState } from "react";
 import AddTaskButton from "./AddTaskButton";
 
 function Content() {
-  const addTaskHandler = () => {
-    console.log("task added");
-  };
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  function handleChange(e) {
+    setInputValue(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, inputValue]);
+    setInputValue("");
+  }
   return (
     <>
       <div className="flex justify-between space-x-4">
         <input
           type="text"
+          value={inputValue}
+          onChange={handleChange}
           placeholder="Enter your task"
           className="flex-1 border border-purple-500 rounded-md px-4"
         />
-        <AddTaskButton title="Add Task" onPress={addTaskHandler} />
+        <AddTaskButton title="Add Task" onPress={handleSubmit} />
       </div>
+      {todos ? (
+        <ul className="my-10 rounded-md shadow-lg shadow-purple-100 hover:shadow-purple-200">
+          {todos.map((todo) => (
+            <li
+              key={todo}
+              className="flex items-center py-3 px-4 justify-between"
+            >
+              {todo}
+              <div className="flex space-x-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="size-5"
+                >
+                  <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
+                  <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="size-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8.75 1A2.75 2.75 0 0 0 6 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 1 0 .23 1.482l.149-.022.841 10.518A2.75 2.75 0 0 0 7.596 19h4.807a2.75 2.75 0 0 0 2.742-2.53l.841-10.52.149.023a.75.75 0 0 0 .23-1.482A41.03 41.03 0 0 0 14 4.193V3.75A2.75 2.75 0 0 0 11.25 1h-2.5ZM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4ZM8.58 7.72a.75.75 0 0 0-1.5.06l.3 7.5a.75.75 0 1 0 1.5-.06l-.3-7.5Zm4.34.06a.75.75 0 1 0-1.5-.06l-.3 7.5a.75.75 0 1 0 1.5.06l.3-7.5Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mx-auto h-12 w-12 text-gray-400"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33"
+            ></path>
+          </svg>
+        </p>
+      )}
     </>
   );
 }
